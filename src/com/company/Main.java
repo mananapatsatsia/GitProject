a39d194fa55b06aeb5dfe30d1f27481fc0b2e6ac
mananapatsatsia <mananapatsatsiaqa@gmail.com>
2022-05-17 16:11:05 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("I am the first project");
+        System.out.println("I am the first2 project");
+
+        System.out.println("I am the first 1project");
+
+
     }
 }
